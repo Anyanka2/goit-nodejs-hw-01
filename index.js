27c0a697da659f0d130957316026a1e4be4b78b4
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-10-10 14:16:43 +0200

    finished

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-import { listContacts } from "./contacts";
-listContacts();
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+import { program } from "commander";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      console.table(allContacts);
+      break;
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    case "remove":
+      const remove = await removeContact(id);
+      console.log(remove);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
